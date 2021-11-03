fea0330d9ac7efdbc7243c5d9ce26501aab24fea
Eddy Nguyen <eddeee.nguyen@gmail.com>
2021-11-03 23:04:49 +1100

    Add /pokemons route

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const data = require("./data.json");
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/pokemons", (req, res) => {
+  res.json(data);
 });
 
 app.listen(port, () => {

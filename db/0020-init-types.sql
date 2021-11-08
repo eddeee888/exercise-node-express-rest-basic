bb4737d37f58944deed5d06c153d4372b621e040
Eddy Nguyen <eddeee.nguyen@gmail.com>
2021-11-08 21:51:01 +1100

    Add queries to create tables and insert records that match data.json

---
+++ db/0020-init-types.sql
@@ -0,0 +1,24 @@
+INSERT INTO
+  `pokemons`.`types` (`name`)
+VALUES
+  ('Fire');
+INSERT INTO
+  `pokemons`.`types` (`name`)
+VALUES
+  ('Grass');
+INSERT INTO
+  `pokemons`.`types` (`name`)
+VALUES
+  ('Poison');
+INSERT INTO
+  `pokemons`.`types` (`name`)
+VALUES
+  ('Flying');
+INSERT INTO
+  `pokemons`.`types` (`name`)
+VALUES
+  ('Water');
+INSERT INTO
+  `pokemons`.`types` (`name`)
+VALUES
+  ('Bug');
---
+++ db/0040-init-pokemons_types.sql
@@ -0,0 +1,56 @@
+INSERT INTO
+  `pokemons`.`pokemons_types` (`pokemon_id`, `type_id`)
+VALUES
+  (1, 2);
+INSERT INTO
+  `pokemons`.`pokemons_types` (`pokemon_id`, `type_id`)
+VALUES
+  (1, 3);
+INSERT INTO
+  `pokemons`.`pokemons_types` (`pokemon_id`, `type_id`)
+VALUES
+  (2, 2);
+INSERT INTO
+  `pokemons`.`pokemons_types` (`pokemon_id`, `type_id`)
+VALUES
+  (2, 3);
+INSERT INTO
+  `pokemons`.`pokemons_types` (`pokemon_id`, `type_id`)
+VALUES
+  (3, 2);
+INSERT INTO
+  `pokemons`.`pokemons_types` (`pokemon_id`, `type_id`)
+VALUES
+  (3, 3);
+INSERT INTO
+  `pokemons`.`pokemons_types` (`pokemon_id`, `type_id`)
+VALUES
+  (4, 1);
+INSERT INTO
+  `pokemons`.`pokemons_types` (`pokemon_id`, `type_id`)
+VALUES
+  (5, 1);
+INSERT INTO
+  `pokemons`.`pokemons_types` (`pokemon_id`, `type_id`)
+VALUES
+  (6, 1);
+INSERT INTO
+  `pokemons`.`pokemons_types` (`pokemon_id`, `type_id`)
+VALUES
+  (6, 4);
+INSERT INTO
+  `pokemons`.`pokemons_types` (`pokemon_id`, `type_id`)
+VALUES
+  (7, 5);
+INSERT INTO
+  `pokemons`.`pokemons_types` (`pokemon_id`, `type_id`)
+VALUES
+  (8, 5);
+INSERT INTO
+  `pokemons`.`pokemons_types` (`pokemon_id`, `type_id`)
+VALUES
+  (9, 5);
+INSERT INTO
+  `pokemons`.`pokemons_types` (`pokemon_id`, `type_id`)
+VALUES
+  (10, 6);

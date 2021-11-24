91afb91b0d8be9bfa515860eb3c8021b67f6963b
Eddy Nguyen <eddeee.nguyen@gmail.com>
2021-11-24 22:44:15 +1100

    Add knex connections and handle /pokemons logic

---
+++ database/tables.js
@@ -0,0 +1,9 @@
+const tables = {
+  Pokemons: "pokemons",
+  Types: "types",
+  Pokemons_Types: "pokemons_types",
+};
+
+module.exports = {
+  tables,
+};
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const data = require("./data.json");
+const { knex } = require("./database/knex");
+const { tables } = require("./database/tables");
 
 const app = express();
 const port = 3000;
@@ -8,8 +10,35 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/pokemons", (req, res) => {
-  res.json(data);
+app.get("/pokemons", async (req, res) => {
+  try {
+    const pokemons = await knex
+      .from(`${tables.Pokemons} as p`)
+      .leftJoin(`${tables.Pokemons_Types} as pt`, `p.id`, "pt.pokemon_id")
+      .leftJoin(`${tables.Types} as t`, `t.id`, "pt.type_id")
+      .select(
+        "p.pokedex_id as id",
+        "p.name",
+        knex.raw("GROUP_CONCAT(??.?? ORDER BY ??.?? ASC) as types", [
+          "t",
+          "name",
+          "t",
+          "name",
+        ])
+      )
+      .orderBy("p.pokedex_id")
+      .groupBy("p.pokedex_id");
+
+    const result = pokemons.map((pokemon) => ({
+      ...pokemon,
+      types: pokemon.types ? pokemon.types.split(",") : [],
+    }));
+
+    res.json(result);
+  } catch (e) {
+    console.error(e);
+    res.status(500);
+  }
 });
 
 app.get("/pokemons/:pokemonName", (req, res) => {
